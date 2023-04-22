9930f3c4ae7ff907eff6f205267155f1b810962b
Nahzilian <nahz3112@gmail.com>
2023-04-22 13:06:36 -0400

    Nodemon

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const { Client, Events, GatewayIntentBits } = require("discord.js");
 // const { token } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages
+    ],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+});
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on('messageCreate', msg => {
+    // if (msg.content === '!hello') {
+        msg.reply('Hello!');
+    // }
 });
 
 // Log in to Discord with your client's token
